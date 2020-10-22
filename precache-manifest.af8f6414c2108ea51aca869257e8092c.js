d297a9b99dfa079649388ed755cc77ad00f49725
mindfulme <go.go.gg.rostislav@gmail.com>
2020-10-23 00:41:37 +0300

    f

---
+++ precache-manifest.af8f6414c2108ea51aca869257e8092c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f94137d48c0485d9b1f1f42d93cd37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fce0ca0a3c3bbde3ed09",
+    "url": "/static/css/main.00116285.chunk.css"
+  },
+  {
+    "revision": "b7bb8ffe5adb024098dc",
+    "url": "/static/js/2.800674a3.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.800674a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce0ca0a3c3bbde3ed09",
+    "url": "/static/js/main.e329c54f.chunk.js"
+  },
+  {
+    "revision": "249ce9805e5f357a809f",
+    "url": "/static/js/runtime-main.c8adf6a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
